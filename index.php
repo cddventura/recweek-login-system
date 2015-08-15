bad1727653b78f6f4d9a19d0e6299bfabd8b724e
CharlesDVentura <charlesventura@ymail.com>
2015-08-15 09:32:47 +0800

    Added php code about verification of username and password input in index.php

--- index.php
+++ index.php
@@ -54,18 +54,41 @@
     </div>
 
     <?php
-      if ($_POST) {
-        $login = $_POST['login'];
-        $password = $_POST['password'];
-        
-        $hash = "root";
-        if (($password == "root") && ($login == "root")) {
-          header("Location: login.php");
+      session_start();
+
+      if(isset($_SESSION['valid_user'])
+      {
+        header('Location: login.php');
+      }
+      else
+      {
+        if(isset($_POST['login']) && isset(&_POST['password']))
+        {
+          $login = $_POST['login'];
+          $password = $_POST['password'];
+
+          $db = new mysqli('localhost', 'root', 'root', 'Recweek');
+
+          if(mysqli_connect_errno())
+          {
+            echo 'Error: Could not connect to database. Please try again later.';
+            exit;
+          }
+
+          $query = 'select * from userinfo '."where usernmae='$login' "."and password=sha1('$password'";
+          $result = $db->query($query);
+
+          if($result->num_rows)
+          {
+            $_SESSION['valid_user'] = $login;
+            session_set_cookie_params(0);
+            header("Location: login.php");
+          }
         }
-        else {
-          echo '<script> alert("Invalid credentials!"); </script>';
-          exit;
-        }   
+        else
+        {
+          echo '<script> alert("Input username/password");</script>';
+        }
       }
     ?>
 
