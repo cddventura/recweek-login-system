1c2f4c92f02822dc6f8a648c8e5641a9d0639a3e
CharlesDVentura <charlesventura@ymail.com>
2015-08-22 12:24:25 +0800

    fixed final bug at submit.php

--- submit.php
+++ submit.php
@@ -8,12 +8,11 @@
 		$nickname = $_POST['nickname'];
 		$year = $_POST['year'];
 		$course = $_POST['course'];
-		$cellphone_number = $_POST['cellphone_number'];
+		$cellphone_number = (string) $_POST['cellphone_number'];
 		$email = $_POST['email'];
 		$school = $_POST['school'];
 		$memstat = $_POST['memStat'];
 		$paid = $_POST['paid'];
-
 
 		$db->query("INSERT INTO signups (stu_id, stu_full_name, stu_nickname, stu_year, stu_course, stu_cell_number, stu_email, stu_school, stu_memstat, stu_paid)
 		  VALUES ('$id_number', '$full_name', '$nickname', '$year', '$course', '$cellphone_number', '$email', '$school', '$memstat', '$paid')");
